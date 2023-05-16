5694cf3b342f5a594442910f0b07379c29e7f3f3
VisaacG <129190529+VisaacG@users.noreply.github.com>
2023-05-16 12:14:35 -0400

    :,(

---
+++ js nuevo/ejercicios.js/index.js
@@ -0,0 +1,44 @@
+/* numbers */
+/* hallar el valor mayor de dos numeros */
+/* sumar, restar, multiplicar y dividir los siguientes nuemeros */
+let valor1 = prompt ('escribe el primer valor')
+let valor2= prompt('escribe el segundo valor')
+
+function sumar (a, b){
+    parseInt()
+    console.log(a + b)
+}
+
+
+
+/* let valor1= 10
+let valor2= '20'
+
+let resultado1= valor1 + valor2
+let resultado2= valor1 - valor1
+let resultado3= valor2 * valor1
+let resultado4= valor2 / valor2
+console.log(resultado1)
+console.log(resultado2)
+console.log(resultado3)
+console.log(resultado4)
+let calcular= valor1 + Number(valor2)
+console.log(calcular) */
+/* calcular la raiz cuadrada de los numeros agregados */
+/* 1 /* DIDENTIFICAL LA LONGITUS DE LA CADENA DE TEXTOA AGREGAR */
+/* 2INVERTIR LA CADENA DE TEXTO   */
+/* 3 REPETIR TEXTO */
+
+
+
+/* ARRAYS */
+/* 1agregar valores al array al princiopio ["soy", "hola", "Gregory"] */
+/* 3 sumar todos los numeros de los arrays 1,2,3=6 */
+
+/* INTERADORES */
+/* 1 MUESTRAME LOS ELEMENTOS DEL INTERADOR */
+/* ITERAR UN ARRAY DE NUMEROS Y DEVOLVERME EL VALOR*/
+/* MUESTRAME LOS INDICES DEL ARRAY QUE VAS A ITERAR*/
+
+
+
